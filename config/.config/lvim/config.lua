3679e8d2ef40353d0209c7e681ae56290a6ed331
Lossilklauralin <lossilklauralin@gmail.com>
2025-07-14 23:00:34 +0800

    update for nix

---
+++ config/.config/lvim/config.lua
@@ -0,0 +1,39 @@
+-- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Example configs: https://github.com/LunarVim/starter.lvim
+-- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Forum: https://www.reddit.com/r/lunarvim/
+-- Discord: https://discord.com/invite/Xb9B4Ny
+
+lvim.plugins = {
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "rcarriga/nvim-notify",
+    },
+    config = function ()
+      require("noice").setup({
+        lsp = {
+          override = {
+            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+            ["vim.lsp.util.stylize_markdown"] = true,
+            ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+          },
+        },
+        presets = {
+          command_palette = true, -- position the cmdline and popupmenu together
+        },
+        messages = {
+          enabled = false
+        },
+        notify = {
+          enabled = false
+        }
+      })
+    end
+  }
+}
+
+lvim.colorscheme = "catppuccin-frappe"
